6ef7f14af328cc15ee8b97a556773f1e7ea8ecbe
paulmunyao <paulmunyao094@gmail.com>
2023-02-10 12:35:58 +0300

    feat(socket-server.py): add a feature for server to communicate with the client
    The server is able to connect with the client server via a network
    
    BREAKING CHANGE:

--- socket-server.py
+++ socket-server.py
@@ -1,1 +1,26 @@
 import socket
+
+def server_connect():
+    host = socket.gethostname()
+    port = 2700
+
+    server_socket = socket.socket()
+    server_socket.bind((host,port))
+
+    # Configuring the server to hold a maximum number of clients
+    server_socket.listen(100)
+    conn,address = server_socket.accept()
+    print("Connection from:" + str(address))
+
+    while True:
+        data = conn.recv(2700).decode()
+        if not data:
+            break
+        print("Received from user: " + str(data))
+        data = input('Input message:')
+        conn.send(data.encode())
+
+    conn.close()
+
+if __name__ == '__main__':
+    server_connect()    
