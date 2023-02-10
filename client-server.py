7d333ef06d252083f940cb4306fba291d6bc0cc5
paulmunyao <paulmunyao094@gmail.com>
2023-02-10 19:51:05 +0300

    style(socket-server.py-&-client-server.py): Automatic installation of trunk
    trunk helps developers highlight errors where it has occured

--- client-server.py
+++ client-server.py
@@ -1,1 +1,22 @@
+import socket
+
+def client_connect():
+    host = socket.gethostname()
+    port = 2700
+
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+
+    message = input("Add message:")
+
+    while message.lower().strip() != "message":
+        client_socket.send(message.encode())
+        data = client_connect.recv(2700).decode()
+
+        print("Received message from server: " + data)
+
+        message = input("")
+
+        
+                                                                      
 
